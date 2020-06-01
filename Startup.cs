8131e0ca3f698b9a0b4658ebd6e54be405a56867
TaylorDickens <justindickens17@gmail.com>
2020-06-01 17:30:33 -0400

    got data from sql server

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,8 @@ namespace Commander
 
             services.AddControllers();
 
-            services.AddScoped<ICommanderRepo, MockCommanderRepo>();
+            // services.AddScoped<ICommanderRepo, MockCommanderRepo>();
+            services.AddScoped<ICommanderRepo, SqlCommanderRepo>();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
