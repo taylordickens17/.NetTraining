d49d3c956e0d72c8f46296e90f7fde34affeb69f
TaylorDickens <justindickens17@gmail.com>
2020-06-01 21:22:49 -0400

    finished endpoints

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -56,5 +56,23 @@ namespace Commander.Controllers
             return CreatedAtRoute(nameof(GetCommandById), new { Id = commandReadDto.Id }, commandReadDto);
             // return Ok(commandReadDto);
         }
+
+        //PUT api/commands/{id}
+        [HttpPut("{id}")]
+        public ActionResult UpdateCommand(int id, CommandUpdateDto commandUpdateDto)
+        {
+            var commandModelFromRepo = _repository.GetCommandById(id);
+            if (commandModelFromRepo == null)
+            {
+                return NotFound();
+            }
+            _mapper.Map(commandUpdateDto, commandModelFromRepo);
+
+            _repository.UpdateCommand(commandModelFromRepo);
+
+            _repository.SaveChanges();
+
+            return NoContent();
+        }
     }
 }
